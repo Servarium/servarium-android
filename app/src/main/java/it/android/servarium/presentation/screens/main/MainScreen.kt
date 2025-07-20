e7cefce5ccf7eac681dd82a7a54cad8b265fcc37
Trankovan <itrankov2004@gmail.com>
2025-07-20 18:37:34 +0300

    device list with delete section

--- app/src/main/java/it/android/servarium/presentation/screens/main/MainScreen.kt
+++ app/src/main/java/it/android/servarium/presentation/screens/main/MainScreen.kt
@@ -1,6 +1,5 @@
 package it.android.servarium.presentation.screens.main
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 
 import androidx.compose.foundation.lazy.LazyColumn
@@ -12,7 +11,6 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
